d078ff8dea450cc6f09fd822003205fb8639e5df
callmejojo <jingwangjolie@gmail.com>
2020-11-28 21:12:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("Hello World"); // Strings always with quotation signs
+console.log("3433"); // Numbers
+console.log("false"); // boolean
+// This won't run in code
+
+console.log(1 + 1);
+console.log(3 * 5);
+
+console.log("11");
+console.log(11);
+console.log("11"+"11");
+console.log("11+11");
+console.log(11+11);
+
+console.log("stan"==="jolie");
+console.log("11"===11);
+console.log("stan"!=="Stan");
+console.log("stan"!=="stan");
+
+var myName = "Jolie" // camelcase spelling for variables
+var myAge = "99"
+var myFavDrink = "Coffee"
+
+console.log("My name is " + myName)
+console.log("My age is " + myAge)
+console.log("My fav drink is " + myFavDrink +" and my age is " + myAge)
+
+//function part below↓
+
+function sayHello() {
+  console.log("Hello Everyone!")
+}
+
+console.log(myName)
+sayHello()
+
+function kissJojo() {
+  console.log("Mwah!")
+}
+
+console.log("Stan")
+kissJojo()
+
