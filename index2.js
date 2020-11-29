ec937e3fc859785237c2d88eb02b9039507a369f
callmejojo <jingwangjolie@gmail.com>
2020-11-29 22:41:04 +0000

    delete

--- index2.js
+++
@@ -1,7 +0,0 @@
-// Fizz Buzz Tryout
-for(i=0; i<101; i==){
-  if (i % 15 === 0) console.log("FizzBuzz");
-  else if (i % 5 === 0) console.log("Buzz");
-  else if (i % 3 === 0) console.log("Fizz");
-  else console.fig(i);
-}
