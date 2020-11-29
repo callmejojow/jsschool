7809b2c47819cd096b0d835be619273951a63387
callmejojow <73418982+callmejojow@users.noreply.github.com>
2020-11-29 09:29:39 -0500

    Fixed the format probs

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ console.log(3 * 5);
 
 console.log("11");
 console.log(11);
-console.log("11"+"11");
-console.log("11+11");
-console.log(11+11);
+console.log("11" + "11");
+console.log("11 + 11");
+console.log(11 + 11);
 
 console.log("George"==="jolie");
 console.log("11"===11);
@@ -21,56 +21,58 @@ var myName = "Jolie" // camelcase spelling for variables
 var myAge = "99"
 var myFavDrink = "Coffee"
 
-console.log("My name is " + myName)
-console.log("My age is " + myAge)
-console.log("My fav drink is " + myFavDrink +" and my age is " + myAge)
+console.log("My name is " + myName)；
+console.log("My age is " + myAge)；
+console.log("My fav drink is " + myFavDrink +" and my age is " + myAge)；
 
 //function part below↓
 
 function sayHello() {
-  console.log("Hello Everyone!")
+  console.log("Hello Everyone!")；
 }
 
 console.log(myName)
 sayHello()
 
 function kissJojo() {
-  console.log("Mwah!")
+  console.log("Mwah!")；
 }
 
-console.log("George")
-kissJojo()
+console.log("George")；
+kissJojo()；
 
 function whenToGoOut(date) {
-  console.log("I'm going to ask her out " + date)
+  console.log("I'm going to ask her out " + date)；
 }
-whenToGoOut("today.")
+whenToGoOut("today.")；
 
 function subtractTwoNumbers(num1, num2) {
-  console.log(num1 - num2)
+  console.log(num1 - num2)；
 }
-subtractTwoNumbers(99, 10)
+subtractTwoNumbers(99, 10)；
 
 
 function whatIsHisName(name) {
-  if(name === "Stan") {
-  console.log("Tah-dah, you got his name!");
-  } else {
-  console.log("Boo, that's not his name!");
+  if (name === "Stan") {
+    console.log("Tah-dah, you got his name!");
+  } 
+  else {
+    console.log("Boo, that's not his name!");
   }
 }
-whatIsHisName("Tony")
+whatIsHisName("Tony")；
 
 
 function weatherToday(weather) {
-  if(weather === "sunny") {
-  console.log("Enjoy the sun!");
-  } else if(weather === "rainy") {
-  console.log("Better stay at home.");
+  if (weather === "sunny") {
+    console.log("Enjoy the sun!");
+  } 
+  else if (weather === "rainy") {
+    console.log("Better stay at home.");
   } 
   else {
-  console.log("Check the weather!");
+    console.log("Check the weather!");
   }
 }
 
-weatherToday("windy")
+weatherToday("windy")；
